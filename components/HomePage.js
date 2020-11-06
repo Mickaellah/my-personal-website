172acbeaca4734274f9f94a8fffb4033a95742f5
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-11-06 07:57:43 +0300

    Removed elements that are not used and some styles as well.

--- components/HomePage.js
+++ components/HomePage.js
@@ -4,25 +4,14 @@ import Clopedia from '../img/clopedia.JPG';
 export default function HomePage() {
     return (
         <>
-            {/* <input type="checkbox" id="checkbox-menu" /> */}
             <header>
                 <div>
                     <h1>Clopedia Nomenjanahary</h1>
                     <p className="mission">Frontend developer learner.</p>
                 </div>
 
-                {/* <div>
-                    <label className="label_menu" htmlFor="checkbox-menu">Menu</label>
-                </div> */}
             </header>
-            {/* <nav className="navigation_container">
-                <ul className="navigation">
-                    <li><a className="home_page" href="/">Home</a></li>
-                    <li><a href="./about.html">About</a></li>
-                    <li><a href="./portfolio.html">Portfolio</a></li>
-                    <li><a href="./contact.html">Contact</a></li>
-                </ul>
-            </nav> */}
+            
             <main>
                 <header>
                     <h2>
